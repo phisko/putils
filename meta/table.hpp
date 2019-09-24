1be3b9c42802c4108a4bf2d663187995b6a6bf70
Nicolas Phister] <nicolas@phister.fr>
2019-09-24 17:10:18 +0200

    remove unused FlatTuple from table

--- meta/table.hpp
+++ meta/table.hpp
@@ -5,11 +5,9 @@
 #include "type.hpp"
 
 namespace pmeta {
-    // See bottom of file for example use
-    template<typename Tuple, typename FlatTuple>
+    template<typename Tuple>
     struct table {
-        table(Tuple && tuple, FlatTuple && flatTuple)
-                : _tuple(FWD(tuple)), _flatTuple(FWD(flatTuple)) {}
+        table(Tuple && tuple) : _tuple(FWD(tuple)) {}
 
         template<typename Key, typename Func>
         void get_value(Key && key, Func && func);
@@ -18,14 +16,11 @@ namespace pmeta {
         void get_key(Value && value, Func && func);
 
         const Tuple & getKeyValues() const { return _tuple; }
-        const FlatTuple & getFlatKeyValues() const { return _flatTuple; }
 
 		static constexpr auto size = std::tuple_size<Tuple>::value;
-		static constexpr auto flat_size = size * 2;
 
     private:
         Tuple _tuple;
-        FlatTuple _flatTuple;
     };
 
     template<typename ...KeyValues>
@@ -52,7 +47,7 @@ namespace pmeta {
 
         template<std::size_t KPos, std::size_t VPos, typename Key, typename Table, typename Func>
         void get_value(Key && key, Table && table, Func && func, std::index_sequence<>) {
-            throw std::out_of_range("Not found");
+			assert(!"Key not found");
         }
 
         template<std::size_t KPos, std::size_t VPos, typename Key, typename Table, typename Func, std::size_t I, std::size_t ...Is>
@@ -78,15 +73,15 @@ namespace pmeta {
         }
     }
 
-    template<typename Tuple, typename FlatTuple>
+    template<typename Tuple>
     template<typename Key, typename Func>
-    void table<Tuple, FlatTuple>::get_value(Key && key, Func && func) {
+    void table<Tuple>::get_value(Key && key, Func && func) {
         detail::get_value<detail::KeyPos, detail::ValuePos>(FWD(key), _tuple, FWD(func));
     }
 
-    template<typename Tuple, typename FlatTuple>
+    template<typename Tuple>
     template<typename Value, typename Func>
-    void table<Tuple, FlatTuple>::get_key(Value && value, Func && func) {
+    void table<Tuple>::get_key(Value && value, Func && func) {
         detail::get_value<detail::ValuePos, detail::KeyPos>(FWD(value), _tuple, FWD(func));
     }
 
@@ -110,9 +105,8 @@ namespace pmeta {
 
     template<typename ...Args>
     auto make_table(Args && ...args) {
-        return table<decltype(detail::make_table(args...)), decltype(std::make_tuple(FWD(args)...))>(
-                detail::make_table(args...),
-                std::make_tuple(FWD(args)...)
+        return table(
+			detail::make_table(args...)
         );
     }
 
