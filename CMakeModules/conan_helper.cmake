da6d1937a3170724c5a832e17f088b7fb02a0491
Nicolas Phister <nicolas@phister.fr>
2019-09-26 20:04:18 +0200

    added conan remotes in putils_conan

--- CMakeModules/conan_helper.cmake
+++ CMakeModules/conan_helper.cmake
@@ -3,6 +3,8 @@ include(conan)
 macro(putils_conan)
 # usage: putils_conan(lib/1.0@man/stable)
     conan_check(REQUIRED)
+    conan_add_remote(NAME bincrafters URL https://api.bintray.com/conan/bincrafters/public-conan)
+    conan_add_remote(NAME conan-community URL https://api.bintray.com/conan/conan-community/conan)
 
     conan_cmake_run(
         REQUIRES
