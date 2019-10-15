4c81e802dddb6e786a1db61d17102798be1b2439
Nicolas Phister] <nicolas@phister.fr>
2019-10-15 16:41:10 +0200

    made putils_conan copy dlls to CMAKE_RUNTIME_OUTPUT_DIRECTORY

--- CMakeModules/conan_helper.cmake
+++ CMakeModules/conan_helper.cmake
@@ -10,5 +10,18 @@ macro(putils_conan)
         REQUIRES
             ${ARGV}
         BASIC_SETUP CMAKE_TARGETS
-        BUILD missing)
+        BUILD missing
+        IMPORTS
+            "bin, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
+            "bin, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}"
+            "bin, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}"
+            "bin, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO}"
+            "bin, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL}"
+
+            "lib, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}"
+            "lib, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}"
+            "lib, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}"
+            "lib, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO}"
+            "lib, *.dll -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL}"
+    )
 endmacro()
