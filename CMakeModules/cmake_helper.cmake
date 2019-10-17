b3ab9ad8a46c7b772a7a5735a89e48c2d8de6034
Nicolas Phister] <nicolas@phister.fr>
2019-10-17 08:57:51 +0200

    add putils_get_subdirectories to cmake_helper

--- CMakeModules/cmake_helper.cmake
+++ CMakeModules/cmake_helper.cmake
@@ -1,3 +1,14 @@
+function(putils_get_subdirectories dst_list curdir)
+    file(GLOB children RELATIVE ${curdir} ${curdir}/*)
+    set(dirlist "")
+    foreach(child ${children})
+        if(IS_DIRECTORY ${curdir}/${child})
+            list(APPEND dirlist ${child})
+        endif()
+	endforeach()
+	set(${dst_list} ${dirlist} PARENT_SCOPE)
+endfunction()
+
 function(putils_get_runtime_output_directories dst_list)
 	set(${dst_list}
 		${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
