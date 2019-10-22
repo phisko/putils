a3adbcae784a34db7ed0acb14c5d339a417022f9
Nicolas Phister] <nicolas@phister.fr>
2019-10-22 10:28:57 +0200

    made file_extension return a string_view and added other path helpers

--- file_extension.hpp
+++ file_extension.hpp
@@ -4,20 +4,31 @@
 #include <string.h>
 
 namespace putils {
-	static const char * file_extension(const char * file) {
+	static std::string_view remove_directory(const char * file) {
+		const auto slash = std::string_view(file).find_last_of("/");
+		if (slash == std::string_view::npos)
+			return file;
+		return file + slash + 1;
+	}
+	
+	static std::string_view file_extension(const char * file) {
 		const auto dot = std::string_view(file).find_last_of(".");
 		if (dot == std::string_view::npos)
 			return file;
 		return file + dot + 1;
 	}
 
-	template<size_t N>
-	static putils::string<N> get_directory(const char * file) {
+	static std::string_view get_directory(const char * file) {
 		const auto slash = std::string_view(file).find_last_of("/");
-		assert(slash != std::string_view::npos);
+		if (slash == std::string_view::npos)
+			return file;
+		return std::string_view(file, slash);
+	}
 
-		putils::string<N> ret;
-		strncpy_s(ret, N, file, slash);
-		return ret;
+	static std::string_view remove_extension(const char * file) {
+		const auto dot = std::string_view(file).find_last_of(".");
+		if (dot == std::string_view::npos)
+			return file;
+		return std::string_view(file, dot);
 	}
 }
