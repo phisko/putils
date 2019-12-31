c395ff204d5c5026572a3a527336dde97a3a529f
Nicolas Phister] <nicolas@phister.fr>
2019-12-31 08:35:46 +0100

    cleanup in Directory

--- Directory.hpp
+++ Directory.hpp
@@ -2,7 +2,6 @@
 
 #include "WindowsDirectory.hpp"
 #include "UnixDirectory.hpp"
-#include "runTests.hpp"
 
 namespace putils {
 #ifdef _WIN32
@@ -10,31 +9,4 @@ namespace putils {
 #elif defined(__unix__)
     using Directory = UnixDirectory;
 #endif
-
-    namespace test {
-        inline bool directory() {
-            return putils::runTests(
-                    "for_each", [] {
-                        Directory d(".");
-                        d.for_each([](auto && f) {
-                                       std::cout << f.name << std::endl;
-                                       std::cout << "\t" << f.fullPath << std::endl;
-                                       std::cout << "\t" << (f.isDirectory ? "Dir" : "NotDir") << std::endl;
-                                   }
-                        );
-                        return true;
-                    },
-                    "getFiles", [] {
-                        Directory d(".");
-                        const auto files = d.getFiles();
-                        for (const auto & f : files) {
-                            std::cout << f.name << std::endl;
-                            std::cout << "\t" << f.fullPath << std::endl;
-                            std::cout << "\t" << (f.isDirectory ? "Dir" : "NotDir") << std::endl;
-                        }
-                        return true;
-                    }
-            );
-        }
-    }
 }
