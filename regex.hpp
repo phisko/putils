f50fc23c12e91616df70b2ec37afe838edb3d974
Nicolas Phister] <nicolas@phister.fr>
2020-01-09 09:07:20 +0100

    move regex members to namespace

--- regex.hpp
+++ regex.hpp
@@ -3,20 +3,22 @@
 #include <string>
 #include <regex>
 
-bool operator==(const std::string & str, const std::regex & reg) {
-    return std::regex_match(str.c_str(), reg);
-}
+namespace putils::regex {
+    bool operator==(const char * & str, const std::regex & reg) {
+        return std::regex_match(str, reg);
+    }
 
-std::cmatch operator==(const std::string & str, const std::pair<std::regex, bool> & reg) {
-    std::cmatch m;
-    std::regex_match(str.c_str(), m, reg.first);
-    return m;
-}
+    std::cmatch operator==(const char * str, const std::pair<std::regex, bool> & reg) {
+        std::cmatch m;
+        std::regex_match(str, m, reg.first);
+        return m;
+    }
 
-std::pair<std::regex, bool> operator ""_m(const char * str, size_t) {
-    return std::make_pair(std::regex(str), true);
-}
+    std::pair<std::regex, bool> operator ""_m(const char * str, size_t) {
+        return std::make_pair(std::regex(str), true);
+    }
 
-std::regex operator ""_r(const char * str, size_t) {
-    return std::regex(str);
+    std::regex operator ""_r(const char * str, size_t) {
+        return std::regex(str);
+    }
 }
