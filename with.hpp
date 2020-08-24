cf8b8d85475a2a5dddaf20b44fb1336a0520651e
Nicolas Phister <nicolas@phister.fr>
2020-08-24 12:08:23 +0200

    fix with.hpp example

--- with.hpp
+++ with.hpp
@@ -5,13 +5,17 @@
 // Lets you do things like:
 //
 // kengine::EntityManager em;
-// { putils_with(em.createEntity<kengine::GameObject>("Bob") {
-//      { putils_with(_.attachComponent<kengine::TransformComponent>()) {  // "_" is provided by putils_with
+// {
+//		putils_with(em.createEntity<kengine::GameObject>("Bob");
+//
+//      {
+//			putils_with(_.attachComponent<kengine::TransformComponent>());  // "_" is provided by putils_with
 //          _.boundingBox.position.x = 0;                                    // "_" is provided by putils_with
 //          _.boundingBox.position.y = 1;
-//      }}
+//      }
 //
-//      { putils_with(_.attachComponent<kengine::GraphicsComponent>()) {
+//      {
+//			putils_with(_.attachComponent<kengine::GraphicsComponent>());
 //          _.appearance = "sprite.png";
 //      }}
-// }}
+// }
