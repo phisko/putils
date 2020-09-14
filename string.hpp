a54a38abc64a72b62cb1bdca0d6279a0b9a8ac69
Nicolas Phister <nicolas@phister.fr>
2020-09-14 09:26:06 +0200

    constexpr string + span conversion

--- string.hpp
+++ string.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <string_view>
+#include <span>
 #include "reflection.hpp"
 
 namespace putils {
@@ -20,55 +21,55 @@ namespace putils {
 		static constexpr auto max_size = MaxSize;
 
 	public:
-		string() {
+		constexpr string() {
 			static_assert(MaxSize > 0);
 			_buff[0] = 0;
 		}
 
 		template<typename ...Args>
-		string(const char * format, Args... args) {
+		constexpr string(const char * format, Args... args) {
 			static_assert(MaxSize > 0);
 			_size = snprintf(_buff, MaxSize, format, args...);
 		}
 
-		string(const char * str) : _size(strlen(str)) {
+		constexpr string(const char * str) : _size(strlen(str)) {
 			static_assert(MaxSize > 0);
 			strncpy_s(_buff, str, MaxSize);
 		}
 
-		string(const std::string & s) : _size(s.size()) {
+		constexpr string(const std::string & s) : _size(s.size()) {
 			static_assert(MaxSize > 0);
 			assert(_size < MaxSize);
 			strncpy_s(_buff, s.data(), _size);
 		}
 
-		string(std::string_view s) : _size(s.size()) {
+		constexpr string(std::string_view s) : _size(s.size()) {
 			static_assert(MaxSize > 0);
 			assert(_size < MaxSize);
 			strncpy_s(_buff, s.data(), _size);
 		}
 
-		string & operator=(const char * rhs) {
+		constexpr string & operator=(const char * rhs) {
 			strncpy_s(_buff, rhs, MaxSize);
 			_size = strlen(rhs);
 			return *this;
 		}
 
 		template<size_t N, const char * S>
-		string & operator=(const string<N, S> & rhs) {
+		constexpr string & operator=(const string<N, S> & rhs) {
 			strncpy_s(_buff, rhs.c_str(), MaxSize);
 			_size = rhs.size();
 			return *this;
 		}
 
-		string & operator=(std::string_view rhs) {
+		constexpr string & operator=(std::string_view rhs) {
 			assert(rhs.size() < MaxSize);
 			strncpy_s(_buff, rhs.data(), rhs.size());
 			_size = rhs.size();
 			return *this;
 		}
 
-		string & operator=(const std::string & rhs) {
+		constexpr string & operator=(const std::string & rhs) {
 			assert(rhs.size() < MaxSize);
 			strncpy_s(_buff, rhs.data(), rhs.size());
 			_size = rhs.size();
@@ -76,23 +77,23 @@ namespace putils {
 		}
 
 		template<typename ...Args>
-		void set(const char * format, Args... args) {
+		constexpr void set(const char * format, Args... args) {
 			_size = snprintf(_buff, MaxSize, format, args...);
 		}
 
 		// Reflectible (no overload/template)
-		void assign(const char * s) {
+		constexpr void assign(const char * s) {
 			*this = s;
 		}
 
-		string & operator+=(const char * pRhs) {
+		constexpr string & operator+=(const char * pRhs) {
 			strncat_s(_buff, pRhs, MaxSize - _size);
 			_size += strlen(pRhs);
 			assert(_size < MaxSize);
 			return *this;
 		}
 
-		string & operator+=(std::string_view rhs) {
+		constexpr string & operator+=(std::string_view rhs) {
 			strncat_s(_buff, rhs.data(), rhs.size());
 			_size += rhs.size();
 			assert(_size < MaxSize);
@@ -100,72 +101,72 @@ namespace putils {
 		}
 
 		template<size_t Size, const char * Name>
-		string & operator+=(const string<Size, Name> & rhs) { return *this += rhs.c_str(); }
+		constexpr string & operator+=(const string<Size, Name> & rhs) { return *this += rhs.c_str(); }
 
-		string & operator+=(char rhs) {
+		constexpr string & operator+=(char rhs) {
 			_buff[_size++] = rhs;
 			assert(_size < MaxSize);
 			_buff[_size] = 0;
 			return *this;
 		}
 
-		string & operator+=(int rhs) {
+		constexpr string & operator+=(int rhs) {
 			_size += snprintf(_buff + _size, MaxSize - _size, "%d", rhs);
 			return *this;
 		}
 
-		string & operator+=(float rhs) {
+		constexpr string & operator+=(float rhs) {
 			_size += snprintf(_buff + _size, MaxSize - _size, "%f", rhs);
 			return *this;
 		}
 
-		string & operator+=(size_t rhs) {
+		constexpr string & operator+=(size_t rhs) {
 			_size += snprintf(_buff + _size, MaxSize - _size, "%zu", rhs);
 			return *this;
 		}
 
 #if defined(_MSC_VER) && _MSC_VER > 1920 // MSVC 2019
-		string & operator+=(unsigned int rhs) {
+		constexpr string & operator+=(unsigned int rhs) {
 			_size += snprintf(_buff + _size, MaxSize - _size, "%zu", rhs);
 			return *this;
 		}
 
-		string & operator+=(intptr_t rhs) {
+		constexpr string & operator+=(intptr_t rhs) {
 			_size += snprintf(_buff + _size, MaxSize - _size, "%zu", rhs);
 			return *this;
 		}
 #endif
 
 		template<typename T>
-		string operator+(T rhs) const {
+		constexpr string operator+(T rhs) const {
 			string ret(_buff);
 			return ret += rhs;
 		}
 
-		bool operator==(const char * rhs) const { return strcmp(_buff, rhs) == 0; }
-		bool operator!=(const char * rhs) const { return strcmp(_buff, rhs) != 0; }
+		constexpr bool operator==(const char * rhs) const { return strcmp(_buff, rhs) == 0; }
+		constexpr bool operator!=(const char * rhs) const { return strcmp(_buff, rhs) != 0; }
 
-		bool operator==(const std::string & rhs) const { return *this == rhs.c_str(); }
-		bool operator!=(const std::string & rhs) const { return *this != rhs.c_str(); }
+		constexpr bool operator==(const std::string & rhs) const { return *this == rhs.c_str(); }
+		constexpr bool operator!=(const std::string & rhs) const { return *this != rhs.c_str(); }
 
 		template<size_t Size, const char * Name>
-		bool operator==(const string<Size, Name> & rhs) const { return *this == rhs.c_str(); }
+		constexpr bool operator==(const string<Size, Name> & rhs) const { return *this == rhs.c_str(); }
 		template<size_t Size, const char * Name>
-		bool operator!=(const string<Size, Name> & rhs) const { return *this != rhs.c_str(); }
+		constexpr bool operator!=(const string<Size, Name> & rhs) const { return *this != rhs.c_str(); }
 
-		string substr(size_t start = 0, size_t count = MaxSize) const {
+		constexpr string substr(size_t start = 0, size_t count = MaxSize) const {
 			string ret;
 			strncpy_s(ret._buff, _buff + start, count);
 			ret._size = std::min(strlen(_buff + start), count);
 			return ret;
 		}
 
-		void clear() {
+		constexpr void clear() {
 			_buff[0] = 0;
 			_size = 0;
 		}
 
-		size_t find(const char * substr, size_t pos = 0) const {
+		constexpr size_t find(const char * substr, size_t pos = 0) const {
 			assert(pos <= _size);
 
 			const auto ret = strstr(_buff + pos, substr);
@@ -174,44 +175,47 @@ namespace putils {
 			return ret - _buff;
 		}
 
-		size_t find(char c, size_t pos = 0) const {
+		constexpr size_t find(char c, size_t pos = 0) const {
 			for (size_t i = pos; i < _size; ++i)
 				if (_buff[i] == c)
 					return i;
 			return npos;
 		}
 
-		char at(size_t i) const { return _buff[i]; }
+		constexpr char at(size_t i) const { return _buff[i]; }
 
 	public:
 		using Buffer = value_type[MaxSize];
-		operator const Buffer &() const { return _buff; }
-		operator Buffer &() { return _buff; }
+		constexpr operator const Buffer &() const { return _buff; }
+		constexpr operator Buffer &() { return _buff; }
 
-		operator std::string_view() const { return std::string_view(_buff); }
+		constexpr operator std::string_view() const { return std::string_view(_buff); }
 		
-		char operator[](size_t i) const { return _buff[i]; }
-		char & operator[](size_t i) { return _buff[i]; }
+		constexpr char operator[](size_t i) const { return _buff[i]; }
+		constexpr char & operator[](size_t i) { return _buff[i]; }
 
-		std::string str() const { return _buff; }
-		const Buffer & data() const { return _buff; }
-		const Buffer & c_str() const { return _buff; }
+		constexpr std::string str() const { return _buff; }
+		constexpr const Buffer & data() const { return _buff; }
+		constexpr const Buffer & c_str() const { return _buff; }
 
-		size_t size() const { return _size; }
-		size_t length() const { return _size; }
-		bool empty() const { return _size == 0; }
-		bool full() const { return _size >= MaxSize; }
+		constexpr size_t size() const { return _size; }
+		constexpr size_t length() const { return _size; }
+		constexpr bool empty() const { return _size == 0; }
+		constexpr bool full() const { return _size >= MaxSize; }
 
-		friend std::ostream & operator<<(std::ostream & s, const string & str) {
+		constexpr friend std::ostream & operator<<(std::ostream & s, const string & str) {
 			return s << str._buff;
 		}
 
 	public:
-		const char * begin() const { return _buff; }
-		const char * end() const { return _buff + _size; }
+		constexpr const char * begin() const { return _buff; }
+		constexpr const char * end() const { return _buff + _size; }
 
-		char * begin() { return _buff; }
-		char * end() { return _buff + _size; }
+		constexpr char * begin() { return _buff; }
+		constexpr char * end() { return _buff + _size; }
+
+		constexpr operator std::span<const char>() const { return { _buff, _size }; }
+		constexpr operator std::span<char>() { return { _buff, _size }; }
 
 	private:
 		size_t _size = 0;
@@ -235,25 +239,25 @@ namespace putils {
 #undef refltype
 
 	template<size_t Size, const char * Name>
-	auto begin(const string<Size, Name> & s) { return s.begin(); }
+	constexpr auto begin(const string<Size, Name> & s) { return s.begin(); }
 	template<size_t Size, const char * Name>
-	auto end(const string<Size, Name> & s) { return s.end(); }
+	constexpr auto end(const string<Size, Name> & s) { return s.end(); }
 
 	template<size_t Size, const char * Name>
-	inline bool operator==(const char * lhs, const putils::string<Size, Name> & rhs) { return rhs == lhs; }
+	constexpr inline bool operator==(const char * lhs, const putils::string<Size, Name> & rhs) { return rhs == lhs; }
 	template<size_t Size, const char * Name>
-	inline bool operator!=(const char * lhs, const putils::string<Size, Name> & rhs) { return rhs != lhs; }
+	constexpr inline bool operator!=(const char * lhs, const putils::string<Size, Name> & rhs) { return rhs != lhs; }
 
 	template<size_t Size, const char * Name>
-	inline bool operator==(const std::string & lhs, const putils::string<Size, Name> & rhs) { return rhs == lhs; }
+	constexpr inline bool operator==(const std::string & lhs, const putils::string<Size, Name> & rhs) { return rhs == lhs; }
 	template<size_t Size, const char * Name>
-	inline bool operator!=(const std::string & lhs, const putils::string<Size, Name> & rhs) { return rhs != lhs; }
+	constexpr inline bool operator!=(const std::string & lhs, const putils::string<Size, Name> & rhs) { return rhs != lhs; }
 }
 
 namespace std {
 	template<size_t N, const char * S>
 	struct hash<putils::string<N, S>> {
-		std::size_t operator()(const putils::string<N, S> & s) const {
+		constexpr std::size_t operator()(const putils::string<N, S> & s) const {
 			size_t result = 0;
 			constexpr auto prime = 31;
 			for (const auto c : s)
