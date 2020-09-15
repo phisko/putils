bcbd54197518a4e6b5056fdf9c2ae9ec978acbd0
Nicolas Phister <nicolas@phister.fr>
2020-09-15 14:49:14 +0200

    vulkan -- support polyvox vertex type

--- vulkan/vulkan_helper.hpp
+++ vulkan/vulkan_helper.hpp
@@ -23,63 +23,89 @@ namespace putils::vulkan {
 
 // Impl
 namespace putils::vulkan {
+	template<typename MemberPtr>
+	vk::VertexInputAttributeDescription getVertexAttributeDescription(uint32_t location, MemberPtr member, uint32_t offset = 0) {
+		vk::VertexInputAttributeDescription attribute;
+
+		attribute.binding = 0;
+		attribute.location = location;
+		attribute.offset = offset + (uint32_t)putils::member_offset(member);
+
+		using Member = putils::MemberType<MemberPtr>;
+
+		const auto setFormat = [&](const vk::Format(&formats)[4]) {
+			constexpr auto length = putils::lengthof<Member>();
+			attribute.format = formats[length - 1];
+		};
+
+		using ArraySubType = std::remove_all_extents_t<Member>;
+#define IsArrayOf(T) std::is_same_v<ArraySubType, T>
+		if constexpr (IsArrayOf(float)) {
+			setFormat({
+				vk::Format::eR32Sfloat,
+				vk::Format::eR32G32Sfloat,
+				vk::Format::eR32G32B32Sfloat,
+				vk::Format::eR32G32B32A32Sfloat
+				});
+		}
+		else if constexpr (IsArrayOf(int)) {
+			setFormat({
+				vk::Format::eR32Sint,
+				vk::Format::eR32G32Sint,
+				vk::Format::eR32G32B32Sint,
+				vk::Format::eR32G32B32A32Sint
+				});
+		}
+		else if constexpr (IsArrayOf(unsigned int)) {
+			setFormat({
+				vk::Format::eR32Uint,
+				vk::Format::eR32G32Uint,
+				vk::Format::eR32G32B32Uint,
+				vk::Format::eR32G32B32A32Uint
+				});
+		}
+		else {
+			static_assert(false);
+		}
+#undef IsArrayOf
+
+		return attribute;
+	}
+
 	template<typename T>
 	VertexDescription getVertexDescription() {
-		static_assert(putils::reflection::has_attributes<T>());
-
 		VertexDescription desc;
 
 		desc.binding.binding = 0;
 		desc.binding.stride = sizeof(T);
 		desc.binding.inputRate = vk::VertexInputRate::eVertex;
 
+		static constexpr bool isPolyVoxType = ::detail::has_nested_type_DataType<T>{};
+
 		uint32_t location = 0;
-		putils::reflection::for_each_attribute<T>([&](const char * name, const auto member) {
-			vk::VertexInputAttributeDescription attribute;
-			attribute.binding = 0;
-			attribute.location = location++;
-			attribute.offset = (uint32_t)putils::member_offset(member);
+		if constexpr (isPolyVoxType) {
+			vk::VertexInputAttributeDescription attr;
+			attr.binding = 0;
+			attr.location = location++;
+			attr.format = vk::Format::eR32G32B32Sfloat;
+			attr.offset = offsetof(T, position);
+			desc.attributes.push_back(attr);
 
-			using Member = putils::MemberType<putils_typeof(member)>;
+			attr.location = location++;
+			attr.offset = offsetof(T, normal);
+			desc.attributes.push_back(attr);
 
-			const auto setFormat = [&](const vk::Format(&formats)[4]) {
-				constexpr auto length = putils::lengthof<Member>();
-				attribute.format = formats[length - 1];
-			};
-
-			using ArraySubType = std::remove_all_extents_t<Member>;
-#define IsArrayOf(T) std::is_same_v<ArraySubType, T>
-			if constexpr (IsArrayOf(float)) {
-				setFormat({
-					vk::Format::eR32Sfloat,
-					vk::Format::eR32G32Sfloat,
-					vk::Format::eR32G32B32Sfloat,
-					vk::Format::eR32G32B32A32Sfloat
-					});
-			}
-			else if constexpr (IsArrayOf(int)) {
-				setFormat({
-					vk::Format::eR32Sint,
-					vk::Format::eR32G32Sint,
-					vk::Format::eR32G32B32Sint,
-					vk::Format::eR32G32B32A32Sint
-					});
-			}
-			else if constexpr (IsArrayOf(unsigned int)) {
-				setFormat({
-					vk::Format::eR32Uint,
-					vk::Format::eR32G32Uint,
-					vk::Format::eR32G32B32Uint,
-					vk::Format::eR32G32B32A32Uint
-					});
-			}
-			else {
-				static_assert(false);
-			}
-#undef IsArrayOf
-
-			desc.attributes.push_back(attribute);
+			using Data = typename T::DataType;
+			const auto dataOffset = offsetof(T, data);
+			putils::reflection::for_each_attribute<Data>([&](const char * name, const auto member) {
+				desc.attributes.push_back(getVertexAttributeDescription(location++, member, (uint32_t)dataOffset));
 			});
+		}
+		else {
+			putils::reflection::for_each_attribute<T>([&](const char * name, const auto member) {
+				desc.attributes.push_back(getVertexAttributeDescription(location++, member));
+			});
+		}
 
 		return desc;
 	}
