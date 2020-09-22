aa112b138128fdf5fa91ff8a23a89ef3e702de69
Nicolas Phister <nicolas@phister.fr>
2020-09-22 13:34:03 +0200

    vulkan - use mesh.indexType in draw()

--- vulkan/Mesh.cpp
+++ vulkan/Mesh.cpp
@@ -3,7 +3,7 @@
 namespace putils::vulkan {
 	void draw(vk::CommandBuffer commandBuffer, const Mesh & mesh) {
 		commandBuffer.bindVertexBuffers(0, *mesh.vertexBuffer.buffer, (uint64_t)0);
-		commandBuffer.bindIndexBuffer(*mesh.indexBuffer.buffer, 0, vk::IndexType::eUint16);
+		commandBuffer.bindIndexBuffer(*mesh.indexBuffer.buffer, 0, mesh.indexType);
 		commandBuffer.drawIndexed((uint32_t)mesh.nbIndices, 1, 0, 0, 0);
 	}
 }
