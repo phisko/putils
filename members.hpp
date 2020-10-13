d6fdab54bf2e15aedde889db190b73af0f953ccc
Nicolas Phister <nicolas@phister.fr>
2020-10-13 14:59:51 +0200

    add ObjectType trait

--- members.hpp
+++ members.hpp
@@ -3,19 +3,34 @@
 #include <type_traits>
 
 // Use this when MemberType alias doesn't work
-#define PUTILS_MEMBER_TYPE(T, Member) std::remove_reference_t<decltype(std::declval<T>().*(std::declval<Member>()))>
+#define PUTILS_MEMBER_TYPE(T, MemberPtr) std::remove_reference_t<decltype(std::declval<T>().*(std::declval<MemberPtr>()))>
 
 namespace putils {
+	namespace detail {
+		template<typename MemberPtr>
+		struct MemberTypeWrapper;
+
+		template<typename T, typename Member>
+		struct MemberTypeWrapper<Member T:: *> {
+			using type = Member;
+		};
+	}
+
 	template<typename MemberPtr>
-	struct MemberTypeWrapper;
+	using MemberType = typename detail::MemberTypeWrapper<std::decay_t<MemberPtr>>::type;
 
-	template<typename T, typename Member>
-	struct MemberTypeWrapper<Member T::*> {
-		using type = Member;
-	};
+	namespace detail {
+		template<typename MemberPtr>
+		struct ObjectTypeWrapper;
 
-	template<typename T>
-	using MemberType = typename MemberTypeWrapper<std::decay_t<T>>::type;
+		template<typename T, typename Member>
+		struct ObjectTypeWrapper<Member T:: *> {
+			using type = T;
+		};
+	}
+
+	template<typename MemberPtr>
+	using ObjectType = typename detail::ObjectTypeWrapper<std::decay_t<MemberPtr>>::type;
 
 	template<typename T, typename U>
 	std::ptrdiff_t member_offset(U T::* member) {
