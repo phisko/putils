fa7da840c38cf47091e2c0f8f58666a0eddfdbe4
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-03-03 09:36:03 +0100

    fix chop + add tests

--- chop.cpp
+++ chop.cpp
@@ -3,9 +3,13 @@
 namespace putils {
     std::string_view chop(std::string_view str) noexcept {
         const auto space = " \t\n\r";
-        const auto firstNonSpace = str.find_first_not_of(space);
+
+        auto firstNonSpace = str.find_first_not_of(space);
+        firstNonSpace = firstNonSpace != std::string::npos ? firstNonSpace : 0;
+
         const auto lastNonSpace = str.find_last_not_of(space) + 1;
-        auto ret = str.substr(firstNonSpace != std::string::npos ? firstNonSpace : 0, lastNonSpace);
+
+        auto ret = str.substr(firstNonSpace, lastNonSpace - firstNonSpace);
         while (!ret.empty() && ((ret.front() == '"' && ret.back() == '"') || (ret.front() == '\'' && ret.back() == '\'')))
             ret = ret.substr(1, ret.size() - 2);
         return ret;
---
+++ tests/chop.tests.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "chop.hpp"
+
+TEST(chop, start) {
+    const auto str = putils::chop(" hello");
+    EXPECT_EQ(str, "hello");
+}
+
+TEST(chop, end) {
+    const auto str = putils::chop("hello ");
+    EXPECT_EQ(str, "hello");
+}
+
+TEST(chop, quotes) {
+    const auto str = putils::chop("\"hello\"");
+    EXPECT_EQ(str, "hello");
+}
+
+TEST(chop, singleQuotes) {
+    const auto str = putils::chop("'hello'");
+    EXPECT_EQ(str, "hello");
+}
+
+TEST(chop, all) {
+    const auto str = putils::chop("  \"'hello'\"  ");
+    EXPECT_EQ(str, "hello");
+}
