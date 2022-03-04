0b710a97826248ee65d37ef0ba5aef57b0a8585b
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-03-04 16:27:38 +0100

    runtime_helper: separator defaults to "."

--- reflection_helpers/runtime_helper.hpp
+++ reflection_helpers/runtime_helper.hpp
@@ -6,10 +6,10 @@ namespace putils::reflection::runtime {
     template<typename T>
     const Attributes & getAttributes() noexcept;
     
-	const AttributeInfo * findAttribute(const Attributes & attributes, std::string_view path, std::string_view separator) noexcept;
+	const AttributeInfo * findAttribute(const Attributes & attributes, std::string_view path, std::string_view separator = ".") noexcept;
 
 	template<typename T>
-	const AttributeInfo * findAttribute(std::string_view path, std::string_view separator) noexcept;
+	const AttributeInfo * findAttribute(std::string_view path, std::string_view separator = ".") noexcept;
 }
 
 #include "runtime_helper.inl"
