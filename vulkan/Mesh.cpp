0b546875365480de5d40f398e5fabd9c1c0512c1
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-11-03 08:53:10 +0100

    remove vulkan helper

--- vulkan/Mesh.cpp
+++
@@ -1,9 +0,0 @@
-#include "Mesh.hpp"
-
-namespace putils::vulkan {
-	void draw(vk::CommandBuffer commandBuffer, const Mesh & mesh) {
-		commandBuffer.bindVertexBuffers(0, *mesh.vertexBuffer.buffer, (uint64_t)0);
-		commandBuffer.bindIndexBuffer(*mesh.indexBuffer.buffer, 0, mesh.indexType);
-		commandBuffer.drawIndexed((uint32_t)mesh.nbIndices, 1, 0, 0, 0);
-	}
-}
