206fb1e4abf73d0426bd1fd66b15478f2c1f7d7d
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-11-03 09:12:57 +0100

    add putils_profiling

---
+++ putils_profiling.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifdef PUTILS_PROFILING
+#	include <tracy/Tracy.hpp>
+#	define PUTILS_PROFILING_SCOPE ZoneScoped
+#	define PUTILS_PROFILING_FRAME FrameMark
+#else
+#	define PUTILS_PROFILING_SCOPE
+#	define PUTILS_PROFILING_FRAME
+#endif
