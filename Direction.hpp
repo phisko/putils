a6b681004b65b7235c30cad49d7841d0f61fc482
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-11-15 16:59:04 +0100

    fix compilation in Direction.hpp

--- Direction.hpp
+++ Direction.hpp
@@ -4,13 +4,13 @@
 #include "Point.hpp"
 
 namespace putils {
-    using Direction = Point<int>;
+    using Direction = Point2i;
 
     namespace Directions {
-        static const Point<int> Up = { 0, 1 };
-        static const Point<int> Down = { 0, -1 };
-        static const Point<int> Left = { -1, 0 };
-        static const Point<int> Right = { 1, 0 };
-        static const Point<int> NoDirection = { 0, 0 };
+        static const Direction Up = { 0, 1 };
+        static const Direction Down = { 0, -1 };
+        static const Direction Left = { -1, 0 };
+        static const Direction Right = { 1, 0 };
+        static const Direction NoDirection = { 0, 0 };
     }
 }
