8396e7e1483298e0283ed8c23010c351a829191d
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-01-11 16:18:44 +0100

    automatically generate reflection code

--- putils/reflection_helpers/type_attributes.hpp
+++ putils/reflection_helpers/type_attributes.hpp
@@ -13,10 +13,14 @@
 namespace putils::reflection::runtime {
 	struct attribute_info;
 
+	//! putils reflect all
+	//! used_types: [putils::reflection::runtime::attribute_info]
 	struct PUTILS_CORE_EXPORT type_attributes {
 		std::unordered_map<std::string, const attribute_info *> map;
 	};
 
+	//! putils reflect all
+	//! used_types: [refltype::array_helper, refltype::map_helper]
 	struct PUTILS_CORE_EXPORT attribute_info {
 		std::ptrdiff_t offset = 0;
 		size_t size = 0;
@@ -24,6 +28,7 @@ namespace putils::reflection::runtime {
 
 		type_attributes attributes;
 
+		//! putils reflect all
 		struct PUTILS_CORE_EXPORT array_helper {
 			size_t get_size(const void * attribute) const noexcept {
 				return get_size_impl(attribute);
@@ -53,6 +58,7 @@ namespace putils::reflection::runtime {
 
 		std::optional<array_helper> array;
 
+		//! putils reflect all
 		struct PUTILS_CORE_EXPORT map_helper {
 			size_t get_size(const void * attribute) const noexcept {
 				return get_size_impl(attribute);
@@ -84,60 +90,4 @@ namespace putils::reflection::runtime {
 	};
 }
 
-#define refltype putils::reflection::runtime::type_attributes
-putils_reflection_info {
-	putils_reflection_custom_class_name(putils_attributes);
-	putils_reflection_attributes(
-		putils_reflection_attribute(map)
-	);
-	putils_reflection_used_types(
-		putils_reflection_type(putils::reflection::runtime::attribute_info)
-	);
-};
-#undef refltype
-
-#define refltype putils::reflection::runtime::attribute_info
-putils_reflection_info {
-	putils_reflection_class_name;
-	putils_reflection_attributes(
-		putils_reflection_attribute(offset),
-		putils_reflection_attribute(size),
-		putils_reflection_attribute(attributes),
-		putils_reflection_attribute(array),
-		putils_reflection_attribute(map)
-	);
-	putils_reflection_used_types(
-		putils_reflection_type(refltype::array_helper),
-		putils_reflection_type(refltype::map_helper)
-	);
-};
-#undef refltype
-
-#define refltype putils::reflection::runtime::attribute_info::array_helper
-putils_reflection_info {
-	putils_reflection_class_name;
-	putils_reflection_attributes(
-		putils_reflection_attribute(element_attributes)
-	);
-	putils_reflection_methods(
-		putils_reflection_attribute(get_size),
-		putils_reflection_attribute(get_element),
-		putils_reflection_attribute(for_each)
-	);
-};
-#undef refltype
-
-#define refltype putils::reflection::runtime::attribute_info::map_helper
-putils_reflection_info {
-	putils_reflection_class_name;
-	putils_reflection_attributes(
-		putils_reflection_attribute(key_attributes),
-		putils_reflection_attribute(value_attributes)
-	);
-	putils_reflection_methods(
-		putils_reflection_attribute(get_size),
-		putils_reflection_attribute(get_value),
-		putils_reflection_attribute(for_each)
-	);
-};
-#undef refltype
+#include "type_attributes.reflection.hpp"
