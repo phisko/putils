4f8f8b5a04d359b50d4c241e41c2f758bea4ea12
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-01-11 16:18:44 +0100

    automatically generate reflection code

---
+++ putils/reflection_helpers/type_attributes.reflection.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "type_attributes.hpp"
+#include "putils/reflection.hpp"
+
+#define refltype putils::reflection::runtime::type_attributes
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(map)
+	);
+	putils_reflection_used_types(
+		putils_reflection_type(putils::reflection::runtime::attribute_info)
+	);
+};
+#undef refltype
+
+#define refltype putils::reflection::runtime::attribute_info
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(offset),
+		putils_reflection_attribute(size),
+		putils_reflection_attribute(type),
+		putils_reflection_attribute(attributes),
+		putils_reflection_attribute(array),
+		putils_reflection_attribute(map)
+	);
+	putils_reflection_used_types(
+		putils_reflection_type(refltype::array_helper),
+		putils_reflection_type(refltype::map_helper)
+	);
+};
+#undef refltype
+
+#define refltype putils::reflection::runtime::attribute_info::array_helper
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(element_attributes),
+		putils_reflection_attribute(get_size_impl),
+		putils_reflection_attribute(get_element_impl),
+		putils_reflection_attribute(for_each_impl)
+	);
+	putils_reflection_methods(
+		putils_reflection_attribute(get_size),
+		putils_reflection_attribute(get_element),
+		putils_reflection_attribute(for_each)
+	);
+};
+#undef refltype
+
+#define refltype putils::reflection::runtime::attribute_info::map_helper
+putils_reflection_info {
+	putils_reflection_class_name;
+	putils_reflection_attributes(
+		putils_reflection_attribute(key_attributes),
+		putils_reflection_attribute(value_attributes),
+		putils_reflection_attribute(get_size_impl),
+		putils_reflection_attribute(get_value_impl),
+		putils_reflection_attribute(for_each_impl)
+	);
+	putils_reflection_methods(
+		putils_reflection_attribute(get_size),
+		putils_reflection_attribute(get_value),
+		putils_reflection_attribute(for_each)
+	);
+};
+#undef refltype
