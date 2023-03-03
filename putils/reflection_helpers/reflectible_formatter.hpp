fedb4de378400d310779d1731abeb617ece47b9f
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-03-03 08:29:02 +0100

    fix gcc 10 compilation

--- putils/reflection_helpers/reflectible_formatter.hpp
+++ putils/reflection_helpers/reflectible_formatter.hpp
@@ -11,26 +11,28 @@
 
 #include "enum_formatter.hpp"
 
-template<putils::reflection::with_attributes T>
-struct fmt::formatter<T> {
-	constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
-		return ctx.begin();
-	}
+namespace fmt {
+	template<putils::reflection::with_attributes T>
+	struct formatter<T> {
+		constexpr auto parse(format_parse_context & ctx) -> decltype(ctx.begin()) {
+			return ctx.begin();
+		}
 
-	template <typename FormatContext>
-	auto format(const T & obj, FormatContext& ctx) const -> decltype(ctx.out()) {
-		fmt::format_to(ctx.out(), "(");
+		template<typename FormatContext>
+		auto format(const T & obj, FormatContext & ctx) const -> decltype(ctx.out()) {
+			fmt::format_to(ctx.out(), "(");
 
-		bool first = true;
-		putils::reflection::for_each_attribute(obj, [&](const auto & attr) noexcept {
-			if (!first)
-				fmt::format_to(ctx.out(), ", ");
-			first = false;
+			bool first = true;
+			putils::reflection::for_each_attribute(obj, [&](const auto & attr) noexcept {
+				if (!first)
+					fmt::format_to(ctx.out(), ", ");
+				first = false;
 
-			fmt::format_to(ctx.out(), "{}: {}", attr.name, attr.member);
-		});
+				fmt::format_to(ctx.out(), "{}: {}", attr.name, attr.member);
+			});
 
-		fmt::format_to(ctx.out(), ")");
-		return ctx.out();
-	}
-};
+			fmt::format_to(ctx.out(), ")");
+			return ctx.out();
+		}
+	};
+}
