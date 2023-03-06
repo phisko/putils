3a9b7f184349afc0fe949ce5a269fca0bba41801
Nicolas Phister <nicolas.phister@dont-nod.com>
2023-03-06 09:36:14 +0100

    reflectible_formatter: fix compilation

--- putils/fmt/tests/reflectible_formatter.tests.cpp
+++ putils/fmt/tests/reflectible_formatter.tests.cpp
@@ -3,6 +3,8 @@
 
 // putils
 #include "putils/fmt/fmt.hpp"
+#include "putils/string.hpp"
+#include "putils/vector.hpp"
 
 struct empty {};
 #define refltype empty
@@ -20,20 +22,24 @@ struct values {
 	int i = 42;
 	double d = 84;
 	std::string s = "hello";
+	putils::string<64> ps = "foo";
+	putils::vector<int, 64> pv{ 0, 42, 84 };
 };
 #define refltype values
 putils_reflection_info {
 	putils_reflection_attributes(
 		putils_reflection_attribute(i),
 		putils_reflection_attribute(d),
-		putils_reflection_attribute(s)
+		putils_reflection_attribute(s),
+		putils_reflection_attribute(ps),
+		putils_reflection_attribute(pv)
 	)
 };
 #undef refltype
 
 TEST(reflectible_formatter, values) {
 	const auto str = fmt::format("{}", values{});
-	EXPECT_EQ(str, "{i: 42, d: 84, s: hello}");
+	EXPECT_EQ(str, "{i: 42, d: 84, s: hello, ps: foo, pv: [0, 42, 84]}");
 }
 
 TEST(reflectible_formatter, formattable) {
